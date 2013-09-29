5b590d9243f4ed7ad8ff2de44e2fdf15debc98bc
Jamie White <jamie@jgwhite.co.uk>
2013-09-29 16:08:41 +0100

    Add failing adapter test

--- test/store_test.js
+++ test/store_test.js
@@ -4,3 +4,11 @@ test('Ember Data version', function() {
   ok(DS.VERSION.indexOf('1.0.0-beta') === 0,
      'expected DS.VERSION to be 1.0.0-beta.x (currently ' + DS.VERSION + ')');
 });
+
+test('store uses DS.FixtureAdapter', function() {
+  var store   = App.__container__.lookup('store:main'),
+      adapter = store.adapterFor('application'),
+      name    = adapter.constructor.toString();
+
+  equal(name, 'DS.FixtureAdapter');
+});
