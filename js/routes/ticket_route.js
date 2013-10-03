02ea011bc4621fb2dfbcc9c17aa2be862b93339e
Jamie White <jamie@jgwhite.co.uk>
2013-10-03 07:37:59 +0100

    Add done handler

--- js/routes/ticket_route.js
+++ js/routes/ticket_route.js
@@ -2,6 +2,11 @@ var TicketRoute = Ember.Route.extend({
   actions: {
     edit: function() {
       this.set('controller.isEditing', true);
+    },
+
+    done: function() {
+      this.set('controller.isEditing', false);
+      this.modelFor('ticket').save();
     }
   }
 });
