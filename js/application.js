f3457ac3900b0232a8411009b81ef187cac19ca4
Jamie White <jamie@jgwhite.co.uk>
2013-10-19 23:06:42 +0100

    Handle cancel action

--- js/application.js
+++ js/application.js
@@ -127,6 +127,10 @@ var TicketsNewRoute = Ember.Route.extend({
       var promise = ticket.save();
 
       this.transitionTo('ticket', promise);
+    },
+
+    cancel: function() {
+      this.transitionTo('tickets');
     }
   }
 });
