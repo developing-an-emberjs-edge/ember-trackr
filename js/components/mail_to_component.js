8ff9e488d8ed9fadefcc7381e053335d285a7ed7
Jamie White <jamie@jgwhite.co.uk>
2013-11-11 08:31:38 +0000

    Implement MailToComponent#href

--- js/components/mail_to_component.js
+++ js/components/mail_to_component.js
@@ -1,6 +1,11 @@
 var MailToComponent = Ember.Component.extend({
+  tagName: 'a',
 
+  attributeBindings: ['href'],
+
+  href: function() {
+    return 'mailto:' + this.get('email');
+  }.property('email')
 });
 
 module.exports = MailToComponent;
-
