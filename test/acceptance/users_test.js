7565a08a4c19076698989c1fcc02ff3023305070
Jamie White <jamie@jgwhite.co.uk>
2013-11-11 00:16:09 +0000

    Add failing user gravatar test

--- test/acceptance/users_test.js
+++ test/acceptance/users_test.js
@@ -12,3 +12,13 @@ test('listing users', function() {
        'expected to find Tom Dale');
   });
 });
+
+test('viewing user details', function() {
+  visit('/')
+  .click('a:contains("Users")')
+  .click('a:contains("Tom Dale")')
+  .then(function() {
+    ok(find('img[src^="http://www.gravatar.com/avatar/9bf3a766e037b9d5a4da0a6f9d0f4f68.jpg"]').length,
+       'expected to find gravatar image');
+  });
+});
