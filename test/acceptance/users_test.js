8c8b35a44ab987cc864eb8c524e05e63a07fa87c
Jamie White <jamie@jgwhite.co.uk>
2013-11-17 15:44:37 +0000

    Add failing user editing test

--- test/acceptance/users_test.js
+++ test/acceptance/users_test.js
@@ -22,3 +22,20 @@ test('viewing user details', function() {
        'expected to find gravatar image');
   });
 });
+
+test('editing user details', function() {
+  visit('/users/1')
+  .click('button:contains("Edit")')
+  .fillIn('input[name="firstName"]', 'Tomhuda')
+  .fillIn('input[name="lastName"]', 'Katzdale')
+  .fillIn('input[name="email"]', 'tomster@emberjs.com')
+  .click('button:contains("Done")')
+  .then(function() {
+    ok(find('.list-group-item:contains("Tomhuda Katzdale")').length,
+       'expected title in master list to update');
+    ok(find('.panel-title:contains("Tomhuda Katzdale")').length,
+       'expected title in detail view to update');
+    ok(find('img[src^="http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a.jpg"]').length,
+       'expected gravatar to update');
+  });
+});
