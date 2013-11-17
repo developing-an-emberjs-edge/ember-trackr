a574827110dd2cb3f8480bb14d1a8fe3533fd1ad
Jamie White <jamie@jgwhite.co.uk>
2013-11-17 16:05:00 +0000

    Add done handler to user route

--- js/application.js
+++ js/application.js
@@ -225,6 +225,11 @@ var UserRoute = Ember.Route.extend({
   actions: {
     edit: function() {
       this.set('controller.isEditing', true);
+    },
+
+    done: function() {
+      this.set('controller.isEditing', false);
+      this.modelFor('user').save();
     }
   }
 });
