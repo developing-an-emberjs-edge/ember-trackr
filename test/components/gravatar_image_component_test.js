8dc489b0a405da6cca7e1487e82c2987858b9981
Jamie White <jamie@jgwhite.co.uk>
2013-11-17 15:38:31 +0000

    Add failing GravatarImageComponent test

---
+++ test/components/gravatar_image_component_test.js
@@ -0,0 +1,19 @@
+module('GravatarImageComponent');
+
+test('src with valid email', function() {
+  var component = App.GravatarImageComponent.create({
+    email: 'tom@example.com'
+  });
+
+  equal(component.get('src'),
+        'http://www.gravatar.com/avatar/e4f7cd8905e896b04425b1d08411e9fb.jpg?s=80');
+});
+
+test('src with blank email', function() {
+  var component = App.GravatarImageComponent.create({
+    email: ''
+  });
+
+  equal(component.get('src'),
+        'http://www.gravatar.com/avatar/00000000000000000000000000000000.jpg?s=80');
+});
