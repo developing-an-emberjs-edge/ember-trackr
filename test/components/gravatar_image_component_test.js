8e33c1aa20a72cc020a5822a4e07493fbce528da
Jamie White <jamie@jgwhite.co.uk>
2013-11-17 15:40:10 +0000

    Add failing gravatar component size test

--- test/components/gravatar_image_component_test.js
+++ test/components/gravatar_image_component_test.js
@@ -17,3 +17,13 @@ test('src with blank email', function() {
   equal(component.get('src'),
         'http://www.gravatar.com/avatar/00000000000000000000000000000000.jpg?s=80');
 });
+
+test('src with size set', function() {
+  var component = App.GravatarImageComponent.create({
+    email: 'tom@example.com',
+    size: 512
+  });
+
+  equal(component.get('src'),
+        'http://www.gravatar.com/avatar/e4f7cd8905e896b04425b1d08411e9fb.jpg?s=512');
+});
