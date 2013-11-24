9e2320ff3f781113bce92b4258090d23f41a93ec
Jamie White <jamie@jgwhite.co.uk>
2013-11-24 13:49:16 +0000

    Implement creatorId and assigneeId in TicketController

--- js/application.js
+++ js/application.js
@@ -76,7 +76,25 @@ module.exports = ApplicationController;
 var TicketController = Ember.ObjectController.extend({
   statuses: ['New', 'Open', 'Closed'],
   needs: ['users'],
-  users: Ember.computed.oneWay('controllers.users')
+  users: Ember.computed.oneWay('controllers.users'),
+
+  creatorId: function(key, value) {
+    if (arguments.length === 1) {
+      return this.get('creator.id');
+    } else {
+      var user = this.get('users').findBy('id', value);
+      this.set('creator', user);
+    }
+  }.property('creator.id'),
+
+  assigneeId: function(key, value) {
+    if (arguments.length === 1) {
+      return this.get('assignee.id');
+    } else {
+      var user = this.get('users').findBy('id', value);
+      this.set('assignee', user);
+    }
+  }.property('assignee.id')
 });
 
 module.exports = TicketController;
--- js/controllers/ticket_controller.js
+++ js/controllers/ticket_controller.js
@@ -1,7 +1,25 @@
 var TicketController = Ember.ObjectController.extend({
   statuses: ['New', 'Open', 'Closed'],
   needs: ['users'],
-  users: Ember.computed.oneWay('controllers.users')
+  users: Ember.computed.oneWay('controllers.users'),
+
+  creatorId: function(key, value) {
+    if (arguments.length === 1) {
+      return this.get('creator.id');
+    } else {
+      var user = this.get('users').findBy('id', value);
+      this.set('creator', user);
+    }
+  }.property('creator.id'),
+
+  assigneeId: function(key, value) {
+    if (arguments.length === 1) {
+      return this.get('assignee.id');
+    } else {
+      var user = this.get('users').findBy('id', value);
+      this.set('assignee', user);
+    }
+  }.property('assignee.id')
 });
 
 module.exports = TicketController;
